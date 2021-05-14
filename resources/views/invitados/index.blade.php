92b0e59b31c7f83e2892eab0b506db5b0322ce57
Nestor Villafañe Mass <mass.nestor@gmail.com>
2021-05-14 11:25:17 -0500

    Correccion de imagen de tarjetas

--- resources/views/invitados/index.blade.php
+++ resources/views/invitados/index.blade.php
@@ -8,15 +8,15 @@
 
     <div class="container">
         <h1 class="text-center">Listado de productos</h1>
-        <div class="row">
+        <div class="row center">
                 @foreach ($productos as $producto)
                     @if ($producto->estado_producto)
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="{{ asset('storage'.'/'.$producto->imagen_producto) }}"  alt="...">
+                            <img class="card-img-top"  src="{{ asset('storage'.'/'.$producto->imagen_producto) }}"  alt="{{$producto->nom_producto}}">
                         <div class="card-body">
                             <h5 class="card-title">{{$producto->nom_producto}}</h5>
-                            <p class="card-text">Precio: $.{{$producto->precio_producto}}</p>
-                            <p class="card-text">Cantidad Disp. {{$producto->stock_producto}}</p>
+                            <p class="card-text">Precio: ${{$producto->precio_producto}}</p>
+                            <p class="card-text">Disponibles: {{$producto->stock_producto}}</p>
                             <div class="row">
                                 
                             </div>
